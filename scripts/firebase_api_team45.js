07083e5657ba39a4070f542a97132cda6e2d093b
HEATHERG LEE <leeh1102@gmail.com>
2021-03-12 17:22:27 -0800

    fix login and main html

--- scripts/firebase_api_team45.js
+++ scripts/firebase_api_team45.js
@@ -1,12 +1,11 @@
-
 // Your web app's Firebase configuration
 var firebaseConfig = {
-    apiKey: "AIzaSyDdAt4QxorKQI1gypA8YRQZQx1M4dzxLeU",
-    authDomain: "comp1800-team45.firebaseapp.com",
-    projectId: "comp1800-team45",
-    storageBucket: "comp1800-team45.appspot.com",
-    messagingSenderId: "368836451385",
-    appId: "1:368836451385:web:a6b15b5158c3b0b5e801e8"
+    apiKey: "AIzaSyAcS1imL9a33uTPgLFYx-3KKEQgKqrZNKg",
+    authDomain: "comp1800-team45-02.firebaseapp.com",
+    projectId: "comp1800-team45-02",
+    storageBucket: "comp1800-team45-02.appspot.com",
+    messagingSenderId: "348396662166",
+    appId: "1:348396662166:web:82c6de759eceb968e201b7"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
